251d3d43ab111b9b5a598553657aa0798231a57d
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-03 15:20:07 -0500

    complete recursion method exercise

--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Sample
+{
 
+    public double Factorial(int number)
+    {
+
+        if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return number * Factorial(number - 1);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main()
+        {
+
+            //read number form keyboard
+            Console.WriteLine("Enter a Number:");
+            int n = int.Parse(System.Console.ReadLine());
+
+            //create object
+            Sample s = new Sample();
+
+            //call factorial method
+            double fact = s.Factorial(n);
+
+            Console.WriteLine("The Factorial of " + n + " is: " + fact);
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
